38b2d4358f5e5f26b15dcf090add4602902e930f
Summer Leigh Healey-Lambert <summerleigh.healey@gmail.com>
2020-11-20 15:15:21 -0500

    Created burger application using example cat application from class exercises and exchanging sleepy for devoured and cats for burgers. Added delpoyment for heroku information and env file.

---
+++ config/connection.js
@@ -0,0 +1,26 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+var connection;
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(proces.env.JAWSDB_URL);
+} else {
+    connection =mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "UNCb00tcamp!",
+    database: "burgers_db"
+  });
+}
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Hawiian Burger');
+INSERT INTO burgers (burger_name) VALUES ('BBQ Bacon Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Egg Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('The King', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Not-A-Burger Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Original Cheeseburger');
